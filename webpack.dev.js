4ab54a89de25ea8c8f9b6bb49efa183d188406e6
Umar Khokhar <ujkhokhar@gmail.com>
2017-10-30 17:54:13 -0400

    Add webpack dashboard

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
+  plugins: [
+    new DashboardPlugin({ port: 8080 }),
+  ],
 });
