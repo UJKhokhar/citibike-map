b41a03fc9067c3245a0bf3ce3c46963f735706af
Umar Khokhar <ujkhokhar@gmail.com>
2017-11-02 23:04:27 -0400

    Add delay before showing loading message

--- src/loadables/LoadableStationMap.jsx
+++ src/loadables/LoadableStationMap.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Loadable from 'react-loadable';
 
-function Loading() {
-  return <div>Loading...</div>;
+function Loading(props) {
+  if (props.error) {
+    return <div>Error!</div>
+  } else if (props.pastDelay) {
+    return <div>Loading...</div>;
+  } else {
+    return null;
+  }
 }
 
 const LoadStationMap = Loadable({
   loader: () => import('../components/StationMap'),
   loading: Loading,
+  delay: 300,
 });
 
 export default class LoadableStationMap extends Component {
--- src/loadables/LoadableTripMap.jsx
+++ src/loadables/LoadableTripMap.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Loadable from 'react-loadable';
 
-function Loading() {
-  return <div>Loading...</div>;
+function Loading(props) {
+  if (props.error) {
+    return <div>Error!</div>
+  } else if (props.pastDelay) {
+    return <div>Loading...</div>;
+  } else {
+    return null;
+  }
 }
 
 const LoadTripMap = Loadable({
   loader: () => import('../components/TripMap'),
   loading: Loading,
+  delay: 300,
 });
 
 export default class LoadableTripMap extends Component {
